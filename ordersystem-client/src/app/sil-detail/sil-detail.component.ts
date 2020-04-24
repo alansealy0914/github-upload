2de64e35b2d717b8d552d852438fdf1b62dfe896
alan sealy <alan.sealy@gmail.com>
2020-04-23 19:49:00 -0500

    formatted sil-detail, formatted sils.component

--- ordersystem-client/src/app/sil-detail/sil-detail.component.ts
+++ ordersystem-client/src/app/sil-detail/sil-detail.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Sil } from '../sils/sil.interface';
 
 @Component({
   selector: 'app-sil-detail',
   templateUrl: './sil-detail.component.html',
   styleUrls: ['./sil-detail.component.css']
 })
+
 export class SilDetailComponent implements OnInit {
+  sil: Sil;
+  
+  constructor(private route: ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.data.subscribe((data: { sil: Sil }) => {
+        this.sil = data.sil;
+    });
+    
   }
-
 }
+
+
+
+
+
+
+
+
+   
+    
+
+  
+    
+
+        
+
+    
