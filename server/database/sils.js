c3df8fe450e87a1fd5129f055c058e59dde4ebd2
alan sealy <alan.sealy@gmail.com>
2020-04-27 09:51:07 -0500

    updated product images

--- server/database/sils.js
+++ server/database/sils.js
@@ -5,7 +5,7 @@ var sils = [
         name: 'SkyTec Starter',
         description: 'Twin engine Cessna Piper left wing diesel engine starter',
         notes: 'Use only with commercial jet-diesel engines. Article:X-2234-1123-4',
-        date: 'January 17, 2020',
+        date: 'Jan 17, 2020',
         update: 'none', 
         imageUrl: "../assets/img/sky-tec-starter-200.png" 
     },
@@ -15,29 +15,29 @@ var sils = [
         name: 'LW-Starter Widget',
         description: 'Left wing starter harness',
         notes: 'Use only with commercial jet-diesel engines. Article:X-2276-2014-4',
-        date: 'January 17, 2020',
+        date: 'Jan 17, 2020',
         update: 'none',
         imageUrl: "../assets/img/saieg73r5_a0.jpg"
     },
     {
         id: 1003,
         xref: '6RWF',
-        name: 'Right Wing Flap',
+        name: 'Airplane Tug',
         description: 'Boeing 747 Airplane Tug',
         notes: 'Use only with commercial jet-diesel engines. Article:X-2234-2234-4',
-        date: 'January 17, 2020',
+        date: 'Jan 17, 2020',
         update: 'none',
         imageUrl: "../assets/img/Airplane_Tug.jpg"
     },
     {
         id: 1004,
         xref: 'TB2020',
-        name: 'Collectible Pilot Tote Bag',
-        description: 'Sandy brown shoulder bag with twin pouch',
+        name: 'Outrunner Motor',
+        description: 'Power 360 Brushless Outrunner Motor, 180Kv',
         notes: 'Use only with commercial jet-diesel engines. Article:X-2234-1123-4',
-        date: 'January 17, 2020',
+        date: 'Jan 17, 2020',
         update: 'none',
-        imageUrl: "../assets/img/ar6201.jpg"
+        imageUrl: "../assets/img/eflm4360a_a0.jpg"
     }
 ];
 
