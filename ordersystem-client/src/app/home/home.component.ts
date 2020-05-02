1c7cad7f97cc744faa5c887284c3f785bfb212ba
alan sealy <alan.sealy@gmail.com>
2020-05-02 14:02:15 -0500

    changed label header

--- ordersystem-client/src/app/home/home.component.ts
+++ ordersystem-client/src/app/home/home.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 export class HomeComponent {
     title: string;
     constructor(){
-        this.title = 'APOS ADMINISTRATOR';
+        this.title = 'Admin Portal';
     }
 }
